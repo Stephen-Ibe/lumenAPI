bdce633b6a24276b0164c9fb9dc5ac3baf39430a
KalsonSaint <saintkalson@gmail.com>
2020-02-07 14:40:30 +0100

    update

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function showAllAuthors(){
+        return response()->json(Author::all());
+    }
+
+    public function showOneAuthor($id){
+        return response()->json(Author::find($id));
+    }
+
+    public function create(Request $request){
+        // validation
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required'
+        ]);
+
+        // insert record
+        $author = Author::create($request->all());
+        return response()->json($author, 201);
+    }
+
+    public function update($id, Request $request){
+        // validation
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required'
+        ]);
+
+        // update record
+        $author = Author::findOrFail($id);
+        $author->update($request->all());
+        return response()->json($author, 200);
+    }
+
+    public function delete($id){
+        Author::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}
